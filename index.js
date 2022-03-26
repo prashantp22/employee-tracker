c43c23bf3111c8a62e62bfc5ae813d8404567e42
PrashantP <prashantpatel622@gmail.com>
2022-03-26 15:30:08 -0400

    initial set up

---
+++ index.js
@@ -0,0 +1,12 @@
+const inquirer = require("inquirer");
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'password',
+      database: 'employees'
+    },
+    console.log('Connected to the election database.')
+);
